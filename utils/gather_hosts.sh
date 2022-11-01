2028430b197df2f8a43ef9ef3dbc5dd6d1243954
Birbatron <mungarifr@gmail.com>
2022-11-01 20:18:54 +0100

    Added pipeline?

--- utils/gather_hosts.sh
+++ utils/gather_hosts.sh
@@ -2,8 +2,24 @@
 
 # vars
 basedir=$(dirname $0)
-echo "$basedir"
 hosts_file="$basedir/../ansible/inventory/hosts"
+
+sp="/-\|"
+sc=0
+spin() {
+   printf "\b${sp:sc++:1}"
+   ((sc==${#sp})) && sc=0
+}
+
+ip_aval=1
+echo "Wait for the IPs to become available..."
+while [[ $ip_aval -ne 0 ]]; do
+if [[ $(sudo virsh net-dhcp-leases default | wc -l) -le 3 ]]; then
+    spin
+else
+    ip_aval=0
+fi
+
 echo "[master]" > $hosts_file
 echo "[worker]" >> $hosts_file
 for instance in $(sudo virsh list --all | grep k8s | awk {'print $2'} | sort); do
